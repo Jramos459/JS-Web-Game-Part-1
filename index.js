ff4813f684b2e95ca6858d68df50eaf40c434369
Jramos459 <jramos459@gmail.com>
2023-09-24 21:40:15 -0700

    code with comments

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
-function tile(url, left, bottom, width, height){
-    for(let h = 0; h < height; h++){
-        for(let w = 0; w < width; w++){
-            newImage(url, left + w*100, bottom+h*100)
-        }
-    }
-}
 
-function newImage(url, left, bottom){
-    let object = document.createElement('img')
-    object.src = url
-    object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom + 'px'
-    document.body.append(object)
+// refactor code using one function
+function newImage(url, left, bottom){   // created a function called newImage
+    let object = document.createElement('img')  //creates the image with the name object
+    object.src = url  // specifies the source for the url image
+    object.style.position = 'fixed'  //we give the object a fixed position
+    object.style.left = left + 'px' // we give the object a left direction in pixels
+    object.style.bottom = bottom + 'px' // we give the onject a bottom direction in pixels 
+    document.body.append(object) // places the object on the screen
     return object
 }
 
-function newItem(url, left, bottom){
+function newItem(url, left, bottom){   // defined a function called newItem
     let object = newImage(url, left, bottom)
 
-    object.addEventListener('dblclick', () => {
+    object.addEventListener('dblclick', () => {  // added dblclick to event listener
         object.remove()
     })
 }
 
-let horizon = window.innerHeight / 1.75
-let heightOfSky = window.innerHeight-horizon
-let heightOfGrass = horizon
-
-tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
-tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
-
-
-newImage('assets/green-character.gif', 100, 100)
-newImage('assets/tree.png', 200, 300)
+// coming out of the assets folder
+newImage('assets/green-character.gif', 175, 260)
+newImage('assets/tree.png', 80, 300)
 newImage('assets/pillar.png', 350, 100)
-newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/pine-tree.png', 500, 200)
 newImage('assets/crate.png', 150, 200)
-newImage('assets/well.png', 500, 425)
+newImage('assets/well.png', 200, 425)
+newImage('assets/pine-tree.png', 200, 230)
+newImage('assets/pine-tree.png', 220, 20)
+newImage('assets/boulder.png', 600, 60)
+newImage('assets/red-character.gif', 400, 50)
+newImage('assets/rock.png', 180, 22)
+newImage('assets/tree.png', 325, 425)
 
-newItem('assets/sword.png', 500, 405)
-newItem('assets/shield.png', 165, 185)
-newItem('assets/staff.png', 600, 100)
+
+newItem('assets/sword.png', 525, 405)
+newItem('assets/shield.png', 80, 125)
+newItem('assets/staff.png', 280, 400)
